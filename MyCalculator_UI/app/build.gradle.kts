7ebe3b633d0e1a4803e4a3853ede0767bd62f96e
Gia-Huy Do Hust <huydogiaccac@gmail.com>
2024-11-19 21:35:51 +0700

    Update build.gradle.kts

--- MyCalculator_UI/app/build.gradle.kts
+++ MyCalculator_UI/app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.mycalculator_ui"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.mycalculator_ui"
         minSdk = 27
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
