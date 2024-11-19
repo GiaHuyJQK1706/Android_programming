f097d9935c7bacdff858a02febd54e3804031b3d
Gia-Huy Do Hust <huydogiaccac@gmail.com>
2024-11-19 21:37:38 +0700

    Update build.gradle.kts

--- CurrencyConv/app/build.gradle.kts
+++ CurrencyConv/app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.currencyconv"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.currencyconv"
         minSdk = 27
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
